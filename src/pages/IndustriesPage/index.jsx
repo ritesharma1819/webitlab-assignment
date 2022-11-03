9f170d6f71dc54a7498cb41c1a5f6dba174a29d2
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-11-03 13:17:39 +0530

    done with measurment and industries ui

--- src/pages/IndustriesPage/index.jsx
+++ src/pages/IndustriesPage/index.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import media_image from "../../assets/Images/media.svg";
+import health_image from "../../assets/Images/health.svg";
+import hospitality_image from "../../assets/Images/hospitality.svg";
+import retail_image from "../../assets/Images/retail.svg";
+import education_image from "../../assets/Images/eduation.svg";
+import travel_image from "../../assets/Images/travel.svg";
+import mobile_image from "../../assets/Images/mobile.svg";
+import it_image from "../../assets/Images/IT.svg";
+import agritech_image from "../../assets/Images/agritech.svg";
 
 const IndustriesPage = () => {
+  const industriesData = [
+    {
+      title: "MEDIA & ENTERTAINMENT",
+      image: media_image,
+    },
+    {
+      title: "HEALTH CARE & WELLNESS",
+      image: health_image,
+    },
+    {
+      title: "HOSPITALITY & REAL ESTATE",
+      image: hospitality_image,
+    },
+    {
+      title: "RETAIL & E COMMERCE",
+      image: retail_image,
+    },
+    {
+      title: "EDUCATION & LEARNING",
+      image: education_image,
+    },
+    {
+      title: "TRAVEL & TRANSPORT",
+      image: travel_image,
+    },
+    {
+      title: "MOBILE & TELECOM",
+      image: mobile_image,
+    },
+    {
+      title: "IT & ITES",
+      image: it_image,
+    },
+    {
+      title: "AGRITECH",
+      image: agritech_image,
+    },
+  ];
   return (
-    <div>IndustriesPage</div>
-  )
-}
+    <div className="industries">
+      <div className="industries__title">
+        <h1>WE ARE WORKING </h1>
+        <h1 className="industries__title-success">WITH THESE INDUSTRIES</h1>
+        <p>Our process-driven approach keeps us going</p>
+      </div>
+      <div className="industries__content">
+        {industriesData.map((item, i) => {
+          return (
+            <div className="industries__content-item" key={i}>
+              <img src={item.image} alt="" />
+              <h2>{item.title}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default IndustriesPage
+export default IndustriesPage;
--- src/pages/MeasurmentPage/index.jsx
+++ src/pages/MeasurmentPage/index.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 import "./index.css";
+import transprancy_image from "../../assets/Images/transprency.svg";
+import realTime_image from "../../assets/Images/real_time_project.svg";
+import wellStructured_image from "../../assets/Images/well_structrured.svg";
+import aglie_image from "../../assets/Images/angle.svg";
+import onsite_image from "../../assets/Images/onsite.svg";
+import innovationWork_image from "../../assets/Images/innovation_work.svg";
+import client_image from "../../assets/Images/client.svg";
+import dedicated_image from "../../assets/Images/dedicated.svg";
 
 const MeasurmentPage = () => {
-  return <div>MeasurmentPage</div>;
+  const measurmentData = [
+    {
+      title: "TRANSPARENCY AND RELIABILITY",
+      image: transprancy_image,
+    },
+    {
+      title: "REAL TIME PROJECT STATUS",
+      image: realTime_image,
+    },
+    {
+      title: "WELL STRUCTURED COMMUNICATION",
+      image: wellStructured_image,
+    },
+    {
+      title: "AGILE METHODOLOGY",
+      image: aglie_image,
+    },
+    {
+      title: "ONSITE COLLABORATION",
+      image: onsite_image,
+    },
+    {
+      title: "INNOVATION AT WORK",
+      image: innovationWork_image,
+    },
+    {
+      title: "CLIENT'S INVOLVEMENT IN EACH MILESTONE",
+      image: client_image,
+    },
+    {
+      title: "DEDICATED TEAM",
+      image: dedicated_image,
+    },
+  ];
+  return (
+    <div className="measurment">
+      <div className="measurment__title">
+        <h1>MEASUREMENT</h1>
+        <h1 className="measurment__title-success">TO OUR SUCCESS</h1>
+        <p>Our process-driven approach keeps us going</p>
+      </div>
+      <div className="measurment__content">
+        {measurmentData.map((item, i) => {
+          return (
+            <div className="measurment__content-item" key={i}>
+              <img src={item.image} alt="" />
+              <h2>{item.title}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MeasurmentPage;
